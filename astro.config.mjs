7dbd62368430b9abc29c8e663142f46b025ce315
manuel Cabrera A <manuel920928@gmail.com>
2026-09-09 16:16:00 -0400

    configuracion astro  y depoy github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://importcontrol-demo.pages.dev',
+  site: 'https://manuelDk8s.github.io',
+  base: '/webimportcontrol',
   integrations: [sitemap()],
   output: 'static',
   image: {
